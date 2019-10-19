c82282d3d39f2898ebf7c44d6ff0bbd2ac3bccd1
fobabett <astronomemusic@gmail.com>
2019-10-19 14:19:24 -0700

    vote button text

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,8 @@
   var ballotDescrition = document.getElementById('ballot-description')
   var ballotTitle = document.getElementById('ballot-title')
   var hamburger = document.getElementById('hamburger')
+  var voteForButton = document.getElementById('vote-for-button')
+  var voteAgainstButton = document.getElementById('vote-against-button')
 
   const getBallot = () => wa[2018][Math.floor(Math.random(wa[2018].length)*wa[2018].length)]
 
@@ -10,6 +12,8 @@
     let ballot = getBallot()
     ballotDescrition.innerHTML = ballot.description
     ballotTitle.innerHTML = ballot.title
+    voteForButton.innerHTML = ballot.vote.for
+    voteAgainstButton.innerHTML = ballot.vote.against
   }
 
   renderBallot()
