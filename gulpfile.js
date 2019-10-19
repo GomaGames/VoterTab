9133ab4e60ca1e18d4c441b0b9d6ebae59a8d451
fobabett <astronomemusic@gmail.com>
2019-10-19 13:31:09 -0700

    adds gulp serve and gh pages publish

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,19 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var serve = require('gulp-serve');
  
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./dist'));
 });
  
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+ 
+gulp.task('serve', serve('dist'));
