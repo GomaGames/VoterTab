f0289a02aa31d425ae89dcf1d003e6f97159a724
Jon Borgonia <jon@gomagames.com>
2019-10-19 13:35:41 -0700

    add WA 2018 data

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,15 @@
-var wa = {
-  2018: [
-    {
-      title: "Initiative Measure no. 1631 - Carbon Fee",
-      description: "",
-      readMore: [
-        {
-          title: "Don't call it a tax: Carbon fee heads to ballot",
-          source: "Crosscut",
-          href: "https://crosscut.com/2018/08/dont-call-it-tax-carbon-fee-heads-ballot"
-        },
-      ]
-    }
-  ]
-};
+(() => {
+  var wa = VoterTab.data.wa
+  var ballotDescrition = document.getElementById('ballot-description')
+  var ballotTitle = document.getElementById('ballot-title')
 
-var ballotDescrition = document.getElementById('ballot-description')
-var ballotTitle = document.getElementById('ballot-title')
+  const getBallot = () => wa[2018][Math.floor(Math.random(wa[2018].length)*wa[2018].length)]
 
-const getBallot = () => wa[2018][Math.floor(Math.random(wa[2018].length)*wa[2018].length)]
+  const renderBallot = () => {
+    let ballot = getBallot()
+    ballotDescrition.innerHTML = ballot.description
+    ballotTitle.innerHTML = ballot.title
+  }
 
-const renderBallot = () => {
-  let ballot = getBallot()
-  ballotDescrition.innerHTML = ballot.description
-  ballotTitle.innerHTML = ballot.title
-}
-
-renderBallot()
+  renderBallot()
+})()
